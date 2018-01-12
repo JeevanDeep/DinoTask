d7c802d6401cc2dd0f303aa909f961cc9850aaca
jeevan <jsjeevandeepsingh23@gmail.com>
2018-01-13 02:17:04 +0530

    added progress dialog

--- app/src/main/java/com/gtbit/jeevan/dinotask/activity/DetailActivity.java
+++ app/src/main/java/com/gtbit/jeevan/dinotask/activity/DetailActivity.java
@@ -1,15 +1,16 @@
 package com.gtbit.jeevan.dinotask.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.gtbit.jeevan.dinotask.adapters.MyCommentAdapter;
 import com.gtbit.jeevan.dinotask.ApiService;
-import com.gtbit.jeevan.dinotask.modelClass.CommentData;
 import com.gtbit.jeevan.dinotask.R;
 import com.gtbit.jeevan.dinotask.Utility;
+import com.gtbit.jeevan.dinotask.adapters.MyCommentAdapter;
+import com.gtbit.jeevan.dinotask.modelClass.CommentData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,24 +24,29 @@ public class DetailActivity extends AppCompatActivity {
 
     Retrofit retrofit;
     RecyclerView recyclerView;
+    ProgressDialog progressDialog;
     private int postId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
         postId = getIntent().getIntExtra("postId", 0);
         recyclerView = findViewById(R.id.commentRecycleView);
 
         retrofit = Utility.getRetrofit();
         ApiService apiService = retrofit.create(ApiService.class);
         Call<List<CommentData>> listCall = apiService.getComments();
+        showProgressDialog();
 
         listCall.enqueue(new Callback<List<CommentData>>() {
             @Override
             public void onResponse(Call<List<CommentData>> call, Response<List<CommentData>> response) {
                 List<CommentData> commentDataList = response.body();
                 updateView(commentDataList, postId);
+                progressDialog.dismiss();
             }
 
             @Override
@@ -56,8 +62,15 @@ public class DetailActivity extends AppCompatActivity {
             if (commentData.getPostId() == postId)
                 commentList.add(commentData);
         }
-        MyCommentAdapter adapter = new MyCommentAdapter(commentList,this, postId);
+        MyCommentAdapter adapter = new MyCommentAdapter(commentList, this, postId);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    private void showProgressDialog() {
+        progressDialog = new ProgressDialog(DetailActivity.this);
+        progressDialog.setMax(80);
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
     }
 }
--- app/src/main/java/com/gtbit/jeevan/dinotask/activity/MainActivity.java
+++ app/src/main/java/com/gtbit/jeevan/dinotask/activity/MainActivity.java
@@ -1,15 +1,20 @@
 package com.gtbit.jeevan.dinotask.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
-import com.gtbit.jeevan.dinotask.adapters.MyPostAdapter;
 import com.gtbit.jeevan.dinotask.ApiService;
-import com.gtbit.jeevan.dinotask.modelClass.PostData;
 import com.gtbit.jeevan.dinotask.R;
 import com.gtbit.jeevan.dinotask.Utility;
+import com.gtbit.jeevan.dinotask.adapters.MyPostAdapter;
+import com.gtbit.jeevan.dinotask.modelClass.PostData;
 
 import java.util.List;
 
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     Retrofit retrofit;
     List<PostData> postDataList;
+    ProgressDialog progressDialog;
     private RecyclerView myRecyclerView;
 
     @Override
@@ -33,12 +39,18 @@ public class MainActivity extends AppCompatActivity {
         retrofit = Utility.getRetrofit();
         ApiService apiService = retrofit.create(ApiService.class);
         Call<List<PostData>> call = apiService.getPosts();
+        showProgressDialog();
+        if (!isOnline()) {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+        }
 
         call.enqueue(new Callback<List<PostData>>() {
             @Override
             public void onResponse(Call<List<PostData>> call, Response<List<PostData>> response) {
                 postDataList = response.body();
                 updateView(postDataList);
+                progressDialog.dismiss();
             }
 
             @Override
@@ -48,9 +60,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void showProgressDialog() {
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMax(80);
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+    }
+
     private void updateView(List<PostData> postDataList) {
         MyPostAdapter adapter = new MyPostAdapter(postDataList, MainActivity.this);
         myRecyclerView.setAdapter(adapter);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 }
